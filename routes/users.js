3084c0323d2a44bd88da53b9ee62591cb5ce3060
millouu <srishti.chaurasia123@gmail.com>
2023-12-26 12:28:56 +0530

    add: delete operation

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,8 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24,
-  },
+let users = [
 ];
 
 //all routes in here are starting with /users (defined in index.js)
@@ -20,8 +11,24 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    users.push(req.body);
+    const user = req.body;
+    
+    users.push({...user,id:uuidv4()});
   res.send(`User with the name ${req.body.firstName} added to the database!`);
 });
+
+router.get("/:id", (req, res) => {
+    //destructuring the id from req.params
+    const { id } = req.params;
+    //find the user with the id
+    const found=users.find((user) => user.id === id);
+    res.send(found)
+})
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id !== id)
+    res.send(`User with the id ${id} deleted from the database.`)
+ })
 
 export default router;
