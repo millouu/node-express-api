fd69ac118c9455f5ba2248d90c381c942c21e9a6
millouu <srishti.chaurasia123@gmail.com>
2023-12-26 12:53:35 +0530

    add: update operation

--- routes/users.js
+++ routes/users.js
@@ -29,6 +29,22 @@ router.delete("/:id", (req, res) => {
     const { id } = req.params;
     users = users.filter((user) => user.id !== id)
     res.send(`User with the id ${id} deleted from the database.`)
+})
+ 
+router.patch("/:id", (req, res) => {
+    const { id } = req.params;
+    const userToBeUpdated = users.find((user) => user.id === id);
+    const { firstName, lastName, age } = req.body;
+    if(firstName){
+        userToBeUpdated.firstName = firstName;
+    }
+    if(lastName){
+        userToBeUpdated.lastName = lastName;
+    }
+    if(age){
+        userToBeUpdated.age = age;
+    }
+    res.send(`User with the id ${id} has been updated.`)
  })
 
 export default router;
